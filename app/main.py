e5fd151dc9cccaeeb4527ed1198efc0fca97bc56
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-02-25 12:34:08 +0530

    fixed: exit command to avoid print statement

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ def execute_repl():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        print(f'{command}: command not found') # throw invalid command
 
         if command == "exit 0":
             sys.exit(0)  # Exit with status code 0
 
+        print(f'{command}: command not found') # throw invalid command
 
 def main():
     # Uncomment this block to pass the first stage
