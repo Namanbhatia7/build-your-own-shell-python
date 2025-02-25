f7b7fe6da17db7432547bb1d5177e602117e26b2
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-02-25 12:33:11 +0530

    added: exit command in repl

--- app/main.py
+++ app/main.py
@@ -6,17 +6,15 @@ def execute_repl():
         command = input()
         print(f'{command}: command not found') # throw invalid command
 
+        if command == "exit 0":
+            sys.exit(0)  # Exit with status code 0
+
 
 def main():
     # Uncomment this block to pass the first stage
     # sys.stdout.write("$ ")
 
-    # execute_repl()
-
-    sys.stdout.write("$ ")
-    command = input()
-    print(f'{command}: command not found')
-    sys.exit()
+    execute_repl()
 
 
 if __name__ == "__main__":
