f9bff9b2d74db8b862cd810ac1e85f2004fdb995
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-08 20:05:11 +0530

    fixed: echo command executor

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -4,8 +4,5 @@ from app.commands.base import BaseCommand
 class EchoCommand(BaseCommand):
     def execute(self, args):
         original_command = " ".join(args)
-        
-        # Use shlex to properly parse quotes while keeping spaces inside single quotes
-        tokens = shlex.split(original_command, posix=True)
 
-        print(" ".join(tokens))
+        print(original_command)
