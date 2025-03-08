cd45599d33d03f23db63b702ee68b971a46fb8a6
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-08 19:39:22 +0530

    test: args print

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -2,6 +2,7 @@ from app.commands.base import BaseCommand
 
 class EchoCommand(BaseCommand):
     def execute(self, args):
+        print(args)
         shell_print = " ".join(args)
         shell_print = shell_print.replace("'", "")
         print(shell_print)
