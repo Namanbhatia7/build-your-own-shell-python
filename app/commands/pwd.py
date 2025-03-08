36ce4f819741d4b5a212b970d38c7f6f2c2f1336
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-08 19:23:08 +0530

    added: pwd command handler

---
+++ app/commands/pwd.py
@@ -0,0 +1,9 @@
+from commands.base import BaseCommand
+
+class PwdCommand(BaseCommand):
+    def execute(self, args):
+        """Displays current directory path"""
+        cwd = os.getcwd()
+
+        print(cwd)
+        return
