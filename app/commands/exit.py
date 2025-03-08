4eae2119a54e4ce6d535b6e5c286a1f11e94346a
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-08 19:06:33 +0530

    refactored: exit command

---
+++ app/commands/exit.py
@@ -0,0 +1,7 @@
+import sys
+from app.commands.base import BaseCommand
+
+class ExitCommand(BaseCommand):
+    def execute(self, args):
+        exit_code = int(args[0]) if args and args[0].isdigit() else 0
+        sys.exit(exit_code)
