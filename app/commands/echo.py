b42f1f148fd37417c1b763f12e8e468a7ab7cea4
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-09 13:48:31 +0530

    added: redirect handler in echo command

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -2,7 +2,18 @@ import shlex
 from app.commands.base import BaseCommand
 
 class EchoCommand(BaseCommand):
+    def redirect(self, args):
+        split_index = args.index(">")
+        content = " ".join(args[:split_index])  # Get echo content
+        output_file = args[split_index + 1]  # Get filename
+
+        # Write content to the file
+        with open(output_file, "w") as f:
+            f.write(content + "\n")
+
     def execute(self, args):
         original_command = " ".join(args)
-
-        print(original_command)
+        if ">" in args:
+            self.redirect(args=args)
+        else:
+            print(original_command)
