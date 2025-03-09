5fc39228e326013294c6471da3f1148fd086c170
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-09 14:57:48 +0530

    fixed: type handler

--- app/commands/type.py
+++ app/commands/type.py
@@ -2,6 +2,10 @@ import shutil
 from app.commands.base import BaseCommand
 
 class TypeCommand(BaseCommand):
+    def __init__(self, shell):
+        """Accepts a reference to the Shell instance to check built-in commands."""
+        self.shell = shell
+
     def execute(self, args):
         """Checks if a command is a shell builtin or an executable in PATH."""
         if not args:
@@ -11,7 +15,7 @@ class TypeCommand(BaseCommand):
         input_command = args[0]  # Extract the command name
 
         # Check if the command is a built-in
-        if input_command in self.commands:
+        if input_command in self.shell.commands:
             print(f"{input_command} is a shell builtin")
             return
 
