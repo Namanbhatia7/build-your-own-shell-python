638820aab54d1b42ee80b04ac83528f8b88eb199
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-09 13:54:43 +0530

    updated: redirect symbol handler

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -3,9 +3,10 @@ from app.commands.base import BaseCommand
 
 class EchoCommand(BaseCommand):
     def redirect(self, args):
-        split_index = args.index(">")
-        content = " ".join(args[:split_index])  # Get echo content
-        output_file = args[split_index + 1]  # Get filename
+        redirect_symbol = ">" if ">" in args else "1>"
+        split_index = args.index(redirect_symbol)
+        content = " ".join(args[:split_index])  # Content to echo
+        output_file = args[split_index + 1]  # Filename
 
         # Write content to the file
         with open(output_file, "w") as f:
@@ -13,7 +14,7 @@ class EchoCommand(BaseCommand):
 
     def execute(self, args):
         original_command = " ".join(args)
-        if ">" in args:
+        if ">" in args or "1>" in args:
             self.redirect(args=args)
         else:
             print(original_command)
