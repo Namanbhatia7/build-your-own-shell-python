d5823e61cb9515fc9c3ec470365c94325f0cf99a
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-11 18:52:46 +0530

    fixed: typo for var name

--- app/commands/ls.py
+++ app/commands/ls.py
@@ -21,8 +21,8 @@ class LSCommand(BaseCommand):
         elif "2>" in args:
             stderr_index = args.index("2>")
 
-            dir_args = [arg for arg in args[:split_index] if arg != "-1"]  # Ignore `-1` flag
-            stderr_file = args[split_index + 1]  # Extract output file
+            dir_args = [arg for arg in args[:stderr_index] if arg != "-1"]  # Ignore `-1` flag
+            stderr_file = args[stderr_index + 1]  # Extract output file
 
             if dir_args:
                 path = dir_args[0]  # Take first argument as directory
