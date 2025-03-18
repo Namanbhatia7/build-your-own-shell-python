a1537df44d64b67737b58820f63355d5cf7695aa
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-18 13:17:24 +0530

    cleanup

--- app/main.py
+++ app/main.py
@@ -1,31 +1,3 @@
-# import sys
-
-# from app.service.command_handler import Shell
-
-# def execute_repl():
-#     while True:
-#         sys.stdout.write("$ ")
-#         command = input().strip()
-
-#         if command == "exit 0":
-#             sys.exit(0)  # Exit with status code 0
-
-#         if command.startswith("echo "):
-#             echo_statement = command[5:]
-#             print(echo_statement)
-#         else:
-#             print(f'{command}: command not found') # throw invalid command
-
-# def main():
-#     # Uncomment this block to pass the first stage
-#     # sys.stdout.write("$ ")
-#     shell_executor = Shell()
-#     shell_executor.start()
-
-
-# if __name__ == "__main__":
-#     main()
-
 import sys
 from app.service.command_handler import CommandHandler
 from app.service.history_manager import HistoryManager
