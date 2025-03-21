227e4e1f1b6d0f6dd17fa8ed4a1135422a62e1b7
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-21 15:31:02 +0530

    reverted: original working

--- app/commands/echo.py
+++ app/commands/echo.py
@@ -5,7 +5,7 @@ from app.commands.base import BaseCommand
 class EchoCommand(BaseCommand):
 
     def has_stdout_redirection(self, redirections):
-        return any(redirections[symbol] for symbol in self.REDIRECT_SYMBOLS if (symbol != "2>" or symbol != "2>>"))
+        return any(redirections[symbol] for symbol in self.REDIRECT_SYMBOLS if symbol != "2>")
 
     def handle_redirections(self, redirections, content_str):
         if redirections[">"] or redirections["1>"]:
@@ -17,8 +17,6 @@ class EchoCommand(BaseCommand):
         if redirections["2>"]:
             self.write_to_file(redirections["2>"], "", mode="w", empty_ok=True)
 
-        if redirections["2>>"]:
-            self.write_to_file(redirections["2>>"], content_str, mode="a", stderr=True)
 
     def parse_arguments(self, args):
         """Parses command arguments and returns a tuple of content and redirections."""
@@ -38,20 +36,20 @@ class EchoCommand(BaseCommand):
         redirections, content = self.parse_arguments(args)
         content_str = " ".join(content)
 
+        if redirections["2>>"]:
+            print(content_str, file=sys.stderr)
+
         # Print only if no stdout redirection
         if not self.has_stdout_redirection(redirections):
             print(content_str)
 
         self.handle_redirections(redirections, content_str)
 
-    def write_to_file(self, file_path, content, mode="w", empty_ok=False, stderr=False):
+    def write_to_file(self, file_path, content, mode="w", empty_ok=False):
         """Writes content to a file, ensuring parent directories exist."""
         if content or empty_ok:
             with open(file_path, mode) as f:
                 f.write(content + "\n")
-
-        if stderr:
-            print(content, file=sys.stderr)
 
     def execute(self, args):
         if any(symbol in args for symbol in self.REDIRECT_SYMBOLS):
