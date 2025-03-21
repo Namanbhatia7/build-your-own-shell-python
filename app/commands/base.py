81cddbaca839789251178509d2732635c599cefe
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-21 14:32:44 +0530

    added: 2>> in redirection symbols

--- app/commands/base.py
+++ app/commands/base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class BaseCommand(ABC):
     """Abstract base class for shell commands."""
-    REDIRECT_SYMBOLS = [">", "1>", "2>", ">>", "1>>"]
+    REDIRECT_SYMBOLS = [">", "1>", "2>", ">>", "1>>", "2>>"]
 
     @abstractmethod
     def execute(self, args):
