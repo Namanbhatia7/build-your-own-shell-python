cf3f130920a82ba8ccfad6c884b4c8f5d8660c7a
Namanbhatia7 <bhatia.naman@hotmail.com>
2025-03-26 15:15:18 +0530

    added: tab completion with readline

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from os import read
 import sys
+import readline
 from app.service.command_handler import CommandHandler
 from app.service.history_manager import HistoryManager
+from app.utils.constants import BUILT_IN_COMMANDS
 
 class Shell:
     """Main shell REPL loop."""
@@ -8,6 +11,16 @@ class Shell:
     def __init__(self):
         self.command_handler = CommandHandler()
         self.history_manager = HistoryManager()
+        self.tab_completer()
+    
+    def tab_completer(self):
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(self.completer)
+    
+    def completer(self, text, state):
+        matches = [cmd for cmd in BUILT_IN_COMMANDS if cmd.startswith(text)]
+        return matches[state] if state < len(matches) else None
+
 
     def start(self):
         while True:
